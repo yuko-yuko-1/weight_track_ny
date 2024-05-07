4f958e246ea1aff76a134da9808d11b1febb0749
Naoto-Okumoto <gravity_not@yahoo.co.jp>
2024-05-07 20:45:33 +0900

    [initial-setup/route] - Landing Page Changes
    - Editted route for index.blade.php
    - Editted head of app.blade.php
    - Included 2 pages in index.blade.php
    - Editted 3 controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,12 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    {{-- FontAwesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- css　--}}
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
     <div id="app">
