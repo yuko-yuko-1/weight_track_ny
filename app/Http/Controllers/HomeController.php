9904291bdd65d7d74d18082446cfb32ff9efb307
Naoto-Okumoto <gravity_not@yahoo.co.jp>
2024-05-17 20:08:28 +0900

    [Backend/register] - Backend for pages before logging in
    - modified migration for users table
    - modified User model
    - modified Register and Login Controllers
    - modified some blade pages (register, login, layout/app)
    - made a new route for landing page (weight-and-meals.blade) after logging in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,15 @@ class HomeController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    public function what_is_bmi()
+    {
+        return view('what-is-bmi');
+    }
+
+    public function weight_and_meals()
+    {
+        return view('weight_and_meals.weight_and_meals');
     }
 }
