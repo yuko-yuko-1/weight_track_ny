f1698a03ff5194ed1a5bcb1883e0ffc75bd7935a
Naoto-Okumoto <gravity_not@yahoo.co.jp>
2024-05-28 19:35:44 +0900

    [UI/admin/users] - Created users administration page
    - Frontend for Admin/users page
    - Backend for Admin/users page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
+
+        Gate::define('admin', function($user){
+            return $user->role_id == User::ADMIN_ROLE_ID;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-// use App\Http\Controllers\Auth\LoginController;
+
+use App\Http\Controllers\Admin\UsersController;
 
 Auth::routes();
 
@@ -12,4 +13,14 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('in
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/weight_and_meals/today', [HomeController::class, 'weight_and_meals'])->name('weight_and_meals');
     Route::get('/what-is-bmi', [HomeController::class, 'what_is_bmi'])->name('what-is-bmi');
+
+    ### ADMIN
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function(){
+        #USERS
+        Route::get('/users', [UsersController::class, 'index'])->name('users');
+        Route::delete('/users/{id}/deactivate', [UsersController::class, 'deactivate'])->name('users.deactivate');
+        Route::patch('/users/{id}/activate', [UsersController::class, 'activate'])->name('users.activate');
+        Route::delete('/user/{id}/destroy', [UsersController::class, 'destroy'])->name('users.destroy');
+
+    });
 });
