7963525ade1b00798b415ef5ced91916c83d41f5
narumi1323 <naru.51.5911@gmail.com>
2024-06-19 21:36:24 +0900

    [bugfix2] -Changed logweight-history.blade.php -Changed weight_and_meals.blade.php -Changed weight?editmodal

--- resources/views/weight_and_meals/weight_and_meals.blade.php
+++ resources/views/weight_and_meals/weight_and_meals.blade.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" href="{{ asset('css/weight-and-meals.css')}}">
 <link rel="stylesheet" href="{{ asset('css/calendar.css')}}">
 <link rel="stylesheet" href="{{ asset('css/chart.css')}}">
-<link rel="stylesheet" href="{{ asset('css/goalbar.css')}}">
 <link rel="stylesheet" href="{{ asset('css/Profile/profile.css')}}">
+<link rel="stylesheet" href="{{ asset('css/goalbar.css')}}">
 
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
