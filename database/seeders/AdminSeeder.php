8f5579594433473b717f89eec91a7ca4dabb1ac7
yukonagata <ykubo8@gmail.com>
2024-07-15 15:20:57 +0900

    Update seeders and migrations

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,36 +2,41 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
-    private $user;
+    // private $user;
 
-    public function __construct(User $user)
-    {
-        $this->user = $user;
-    }
+    // public function __construct(User $user)
+    // {
+    //     $this->user = $user;
+    // }
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $this->user->firstname  = 'Admin';
-        $this->user->lastname  = 'Admin';
-        $this->user->username  = 'Admin';
-        $this->user->email = 'admin@gmail.com';
-        $this->user->password = Hash::make('11111111');
-        $this->user->gender = 'Male';
-        $this->user->height = 170;
-        $this->user->prime_weight = 60;
-        $this->user->goal_weight = 65;
-        $this->user->goal_date = '2045-12-31';
-        $this->user->purpose = 'Gain Weight';
-        $this->user->role_id  = User::ADMIN_ROLE_ID;
-        $this->user->save();
+        DB::table('users')->insert([
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'username' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('11111111'),
+            'gender' => 'Male',
+            'height' => 170,
+            'prime_weight' => 60,
+            'goal_weight' => 65,
+            'goal_date' => '2045-12-31',
+            'purpose' => 'Gain Weight',
+            'role_id' => \App\Models\User::ADMIN_ROLE_ID,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        // $this->user->save();
     }
 }
